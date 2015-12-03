0d3b22b9ec8c11077637858a9b7dc566ca2e9694
Grzegorz Kotarski <grzes.kotarski@gmail.com>
2015-12-03 21:42:05 +0100

    pep8 compilance

--- src/weatherserver/model/configmodel.py
+++ src/weatherserver/model/configmodel.py
@@ -15,36 +15,36 @@ class ConfigWeatherModel:
     """
     def __init__(self, temperature, pressure, humidity, windspeed):
         """Constructor of the ConfigWeatherModel class.
-        
+
         :param float temperature: Temperature value [C]
         :param float pressure: Pressure value [hPa].
         :param int humidity: Humidity value [percent].
         :param float windspeed: Wind speed valiue [km/h]
         """
         self._temperature = temperature
-        self._windspeed = windspeed 
+        self._windspeed = windspeed
         self._pressure = pressure
         self._humidity = humidity
         logger().debug('Created {} {}'.format(self.__class__.__name__, str(self)))
-    
+
     @property
     def temperature(self):
         """Temperature getter.
         """
         return self._temperature
-    
+
     @property
     def humidity(self):
         """Humidity getter.
         """
         return self._humidity
-    
+
     @property
     def windspeed(self):
         """Wind speed getter.
         """
         return self._windspeed
-    
+
     @property
     def pressure(self):
         """Pressure getter.
@@ -53,14 +53,14 @@ class ConfigWeatherModel:
 
     def __repr__(self):
         return '{}({},{},{},{})'.format(self.__class__.__name__,
-                                        self.temperature, 
-                                        self.pressure, 
-                                        self.humidity, 
+                                        self.temperature,
+                                        self.pressure,
+                                        self.humidity,
                                         self.windspeed)
 
     def __str__(self):
         return "Temperature={:2.2f} Pressure={:.0f} Humidity={} Windspeed={:2.2f}".format(
-                                        self.temperature, 
-                                        self.pressure, 
-                                        self.humidity, 
+                                        self.temperature,
+                                        self.pressure,
+                                        self.humidity,
                                         self.windspeed)
