b937504bffe527694bd383f4c51b74d53df212a7
kotarskg <grzegorz.kotarski@trw.com>
2015-12-07 09:21:16 +0100

    test refact. cont.

--- src/tests/test_model.py
+++ src/tests/test_model.py
@@ -7,16 +7,28 @@
 import unittest
 from configparser import SafeConfigParser
 
-from weatherserver.config import OPT_HUMIDITY, OPT_PRESSURE, OPT_TEMPERATURE, OPT_WINDSPEED
+from weatherserver.config import (OPT_HUMIDITY,
+                                  OPT_PRESSURE,
+                                  OPT_TEMPERATURE,
+                                  OPT_WINDSPEED,
+                                  SEC_SITE)
 from weatherserver.model.weathermodel import create_weather_model, WeatherModel
+
+TEST_TEMPERATURE_VALUE = 7.4
+TEST_HUMIDITY_VALUE = 45
+TEST_WINDSPEED_VALUE = 15.5
+TEST_PRESSURE_VALUE = 1020
 
 TEST_CONFIG = """
 [site]
-temperature = 7.4
-humidity = 45
-windspeed = 15.5
-pressure = 1020
-"""
+temperature = {}
+humidity = {}
+windspeed = {}
+pressure = {}
+""".format(TEST_TEMPERATURE_VALUE,
+           TEST_HUMIDITY_VALUE,
+           TEST_WINDSPEED_VALUE,
+           TEST_PRESSURE_VALUE)
 
 
 class TestModel(unittest.TestCase):
@@ -26,16 +38,19 @@ class TestModel(unittest.TestCase):
         unittest.TestCase.setUp(self)
         config_parser = SafeConfigParser()
         config_parser.read_string(TEST_CONFIG)
-        self.site_config = config_parser['site']
+        self.site_config = config_parser[SEC_SITE]
 
     def test_weather_model(self):
         """test weather model type
         """
-        weather_model = WeatherModel(10, 20, 30, 40)
-        self.assertEqual(weather_model.temperature, 10)
-        self.assertEqual(weather_model.pressure, 20)
-        self.assertEqual(weather_model.humidity, 30)
-        self.assertEqual(weather_model.windspeed, 40)
+        weather_model = WeatherModel(TEST_TEMPERATURE_VALUE,
+                                     TEST_HUMIDITY_VALUE,
+                                     TEST_WINDSPEED_VALUE,
+                                     TEST_PRESSURE_VALUE)
+        self.assertEqual(weather_model.temperature, TEST_TEMPERATURE_VALUE)
+        self.assertEqual(weather_model.pressure, TEST_HUMIDITY_VALUE)
+        self.assertEqual(weather_model.humidity, TEST_WINDSPEED_VALUE)
+        self.assertEqual(weather_model.windspeed, TEST_PRESSURE_VALUE)
 
     def test_create_weather_model(self):
         """test method creating weather model from config
